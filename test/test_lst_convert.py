99778a67b69f46b4431535a19e969e54bfb41217
Gernot Hillier <gernot.hillier@siemens.com>
2025-04-07 15:07:38 +0200

    lst_to_sbom: add distro qualifier for Alpine
    For Alpine, a package version isn't enough as the same version can have
    different patches in different Alpine branches, so add the `distro`
    qualifier specifying the Alpine release to the PackageURL.
    
    This requires some refactoring as we first need to read all packages and
    guess the Alpine version before adding PackageURLs.
    
    Fixes #3

--- test/test_lst_convert.py
+++ test/test_lst_convert.py
@@ -3,10 +3,10 @@
 #
 # SPDX-License-Identifier: MIT
 
-from capywfa.lst_to_sbom import map_signed_packages
+from capywfa.lst_to_sbom import map_signed_packages, lst_to_sbom
 
 
-def test_signed_packages():
+def test_map_signed_packages():
     assert (map_signed_packages("grub-efi-amd64-signed", "1+2.02+dfsg1+20")
             == ("grub2", "2.02+dfsg1-20"))
 
@@ -18,7 +18,29 @@ def test_signed_packages():
             == ("linux", "4.19.171-2"))
 
 
-def test_signed_packages_shim_deb10u1():
+def test_map_signed_packages_shim_deb10u1():
     assert (map_signed_packages("shim-helpers-amd64-signed",
                                 "1+15+1533136590.3beb971+7+deb10u1")
             == ("shim", "15+1533136590.3beb971-7+deb10u1"))
+
+
+def test_lst_to_sbom_alpine_3_20_apk():
+    bom = lst_to_sbom("apk", "test/fixtures/alpine-3.20-apk-list.txt")
+    assert bom["metadata"]["component"]["pedigree"]["ancestors"][0]["name"] == "Alpine Linux"
+    assert bom["metadata"]["component"]["pedigree"]["ancestors"][0]["version"] == "3.20"
+    assert bom["components"][0]["purl"].startswith("pkg:apk/alpine/alpine-baselayout@3.6.5-r0")
+    assert bom["components"][1]["purl"].startswith("pkg:apk/alpine/alpine-keys@2.4-r1")
+    assert len(bom["components"]) == 4
+    for component in bom["components"]:
+        assert "distro=alpine-3.20" in component["purl"]
+
+
+def test_lst_to_sbom_alpine_3_20_manifest():
+    bom = lst_to_sbom("apk", "test/fixtures/alpine-3.20-manifest.lst")
+    assert bom["metadata"]["component"]["pedigree"]["ancestors"][0]["name"] == "Alpine Linux"
+    assert bom["metadata"]["component"]["pedigree"]["ancestors"][0]["version"] == "3.20"
+    assert bom["components"][0]["purl"].startswith("pkg:apk/alpine/alpine-baselayout@3.6.5-r0")
+    assert bom["components"][1]["purl"].startswith("pkg:apk/alpine/alpine-keys@2.4-r1")
+    assert len(bom["components"]) == 4
+    for component in bom["components"]:
+        assert "distro=alpine-3.20" in component["purl"]
